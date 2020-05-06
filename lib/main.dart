0192d22b01bc569c9f7bf695df3ba2c78c862ae9
fushuaige <1790535171@qq.com>
2020-05-06 14:43:06 +0800

    弹窗

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:daka/pages/ewm.dart';
 import 'package:daka/pages/fans_list_page.dart';
+import 'package:daka/tools/AlertWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -37,32 +38,7 @@ class MyApp1 extends StatelessWidget {
       ),
     ),
   );
-  Widget buttonContainer = Container(
-    // 设置上下左右内边距
 
-    margin: EdgeInsets.only(top: 30),
-    padding: EdgeInsets.only(bottom: 10),
-    width: 90,
-    height: 90,
-    decoration: BoxDecoration(
-      image: new DecorationImage(
-        image: AssetImage("images/gaiz.png"),
-        //这里是从assets静态文件中获取的，也可以new NetworkImage(）从网络上获取
-      ),
-    ),
-
-      child: Center(
-        child: Text(
-          '扫码盖章',
-          style: TextStyle(
-            color: Color.fromRGBO(255, 255, 255, 1.0),
-            fontSize: 14,
-          ),
-          textAlign: TextAlign.center,
-          softWrap: true,
-        ),
-      ),
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -89,129 +65,15 @@ class MyApp1 extends StatelessWidget {
                   margin: EdgeInsets.only(top: 10),
                   height: 310,
                   child: FansListPage()),
-              Container(
-                  child: GestureDetector(
-                      onTap: () {
-                        print('click-OK');
-                        show(context);
-//                        _scan(context);
-//                        Navigator.push(context,
-//                            MaterialPageRoute(builder: (context) => ewMPage()));
-                      },
-                      child: buttonContainer)),
+              Manage(),
             ],
           ),
         ),
       ),
     );
   }
-  Future myDialog(context){
-    return showDialog<Null>(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return GestureDetector(							// 手势处理事件
 
 
-            onTap: (){
-              Navigator.of(context).pop();				//退出弹出框
-            },
-
-
-            child: Container(								//弹出框的具体事件
-              child: Material(
-                color: Color.fromRGBO(0, 0, 0, 0.5),
-                child: Center(
-                  child: Text('具体操作',style: TextStyle(color: Colors.white),),
-                ),
-              ),
-            )
-        );
-
-      },
-    );
-  }
-  show(BuildContext context)
-  {
-//    ScreenUtil.init(context);
-//
-////If the design is based on the size of the iPhone6 ​​(iPhone6 ​​750*1334)
-//    ScreenUtil.init(context, width: 750, height: 1334);
-//
-////If you want to set the font size is scaled according to the system's "font size" assist option
-//    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
-    print('执行6');
-    //Future类型,then或者await获取
-    showDialog(
-      // 传入 context
-        context: context,
-        // 构建 Dialog 的视图
-        builder: (_) => Stack(
-          children: <Widget>[
-            Positioned(
-//              left: ScreenUtil.screenWidth / 10,
-//              top: ScreenUtil.screenWidth / 10 * 3,
-//              child: Image.asset(
-//                'assets/images/updatereminder_icon_bg@3x.png',
-//                width: ScreenUtil.screenWidth / 10 * 8,
-//              ),
-            ),
-            Positioned(
-//              left: ScreenUtil.screenWidth / 10,
-//              top: ScreenUtil.screenWidth / 10 * 3,
-              child: Container(
-                padding: EdgeInsets.only(top: 30, left: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      '${'发现新版本'}',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          decoration: TextDecoration.none),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(left: 3),
-                      child: Text(
-                        '大蘇打',
-                        style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
-                            decoration: TextDecoration.none),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-//                left: ScreenUtil.screenWidth / 10 + 20,
-//                top: ScreenUtil.screenWidth / 10 * 3 + 180,
-                child: Container(
-                  child: Text(
-                    '大蘇打撒',
-                    style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0XFF333333),
-                        decoration: TextDecoration.none),
-                  ),
-                )),
-            Positioned(
-              child: GestureDetector(
-                child: Image.asset('assets/images/updatereminder_icon_close@3x.png',width: 25,),
-                onTap: (){
-                  Navigator.pop(_);
-                },
-              ),
-              ),
-          ],
-        )
-    );
-  }
   Future _scan(BuildContext context) async {
     try {
       String barcode = (await BarcodeScanner.scan()) as String;
@@ -219,7 +81,7 @@ class MyApp1 extends StatelessWidget {
 //        return this.barcode = barcode;
 //      });
       print(barcode+"sjdadhaksd");
-      myDialog(context);
+//      myDialog(context);
     } on PlatformException catch (e) {
       if (e.code == BarcodeScanner.cameraAccessDenied) {
 //        setState(() {
--- lib/tools/AlertWidget.dart
+++ lib/tools/AlertWidget.dart
@@ -1,70 +1,145 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class AlertWidget extends Dialog{
-String title = '';
-String message = '';
-String  confirm = '确定';
-VoidCallback  confirmCallback;
-VoidCallback cancelCallback;
+class Manage extends StatefulWidget {
+  final Widget child;
+  Manage({Key key, this.child}) : super(key: key);
 
-AlertWidget({this.title,this.message,this.cancelCallback,this.confirmCallback,this.confirm});
-@override
-Widget build(BuildContext context) {
+  _ManageState createState() => _ManageState();
+}
+//show(BuildContext context)
+//{
+//
+//  print('执行6');
+//  //Future类型,then或者await获取
+//  showDialog(
+//    // 传入 context
+//      context: context,
+//      // 构建 Dialog 的视图
+//      builder: (_) => Stack(
+//        children: <Widget>[
+//          Positioned(
+//              left: ScreenUtil.screenWidth / 10,
+//              top: ScreenUtil.screenWidth / 10 * 3,
+//            child: Container(
+//              padding: EdgeInsets.only(top: 30, left: 20),
+//              child: Column(
+//                mainAxisAlignment: MainAxisAlignment.start,
+//                crossAxisAlignment: CrossAxisAlignment.start,
+//                children: <Widget>[
+//                  Text(
+//                    '${'发现新版本'}',
+//                    style: TextStyle(
+//                        fontSize: 20,
+//                        fontWeight: FontWeight.bold,
+//                        color: Colors.white,
+//                        decoration: TextDecoration.none),
+//                  ),
+//                  Container(
+//                    padding: EdgeInsets.only(left: 3),
+//                    child: Text(
+//                      '大蘇打',
+//                      style: TextStyle(
+//                          fontSize: 14,
+//                          color: Colors.white,
+//                          decoration: TextDecoration.none),
+//                    ),
+//                  ),
+//                ],
+//              ),
+//            ),
+//          ),
+//          Positioned(
+//                left: ScreenUtil.screenWidth / 10 + 20,
+//                top: ScreenUtil.screenWidth / 10 * 3 + 180,
+//              child: Container(
+//                child: Text(
+//                  '大蘇打撒',
+//                  style: TextStyle(
+//                      fontSize: 14,
+//                      fontWeight: FontWeight.w400,
+//                      color: Color(0XFF333333),
+//                      decoration: TextDecoration.none),
+//                ),
+//              )),
+//        ],
+//      )
+//  );
+//}
+Future myDialog(context){
+  return showDialog<Null>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return GestureDetector(							// 手势处理事件
 
-  return Material(
-//        type: MaterialType.transparency,
-    color: Colors.transparent,
-    shadowColor: Colors.transparent,
-    child: Center(
-      child: Container(
-        margin: EdgeInsets.only(left: 40,right: 40),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            SizedBox(height: 16,),
-            Text(title,style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600),),
-            Text(message),
-            SizedBox(height: 16,),
-            Divider(height: 1,),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: FlatButton(
-                          child: Text('取消'),
-                          onPressed: cancelCallback==null?(){Navigator.pop(context);}:cancelCallback,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border(right: BorderSide(width: 1,color: Color(0xffEFEFF4))),
-                        ),
-                      )
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: FlatButton(
-                      child: Text(confirm,style: TextStyle(color: Colors.black),),
-                      onPressed: (){
-                        Navigator.pop(context);
-                        confirmCallback();
-                      },
-                    ),
-                  )
-                ],
+
+          onTap: (){
+            Navigator.of(context).pop();				//退出弹出框
+          },
+
+
+          child: Container(								//弹出框的具体事件
+            child: Material(
+              color: Color.fromRGBO(0, 0, 0, 0.5),
+              child: Center(
+                child: Text('具体操作',style: TextStyle(color: Colors.white),),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+      );
 
+    },
+  );
+}
+class _ManageState extends State<Manage> {
+  Widget buttonContainer = Container(
+    // 设置上下左右内边距
+
+    margin: EdgeInsets.only(top: 30),
+    padding: EdgeInsets.only(bottom: 10),
+    width: 90,
+    height: 90,
+    decoration: BoxDecoration(
+      image: new DecorationImage(
+        image: AssetImage("images/gaiz.png"),
+        //这里是从assets静态文件中获取的，也可以new NetworkImage(）从网络上获取
+      ),
+    ),
+
+    child: Center(
+      child: Text(
+        '扫码盖章',
+        style: TextStyle(
+          color: Color.fromRGBO(255, 255, 255, 1.0),
+          fontSize: 14,
+        ),
+        textAlign: TextAlign.center,
+        softWrap: true,
       ),
     ),
   );
-}
+  _ManageState();
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context);
+
+//If the design is based on the size of the iPhone6 ​​(iPhone6 ​​750*1334)
+    ScreenUtil.init(context, width: 750, height: 1334);
+
+//If you want to set the font size is scaled according to the system's "font size" assist option
+    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
+   return Container(
+        child: GestureDetector(
+            onTap: () {
+              print('click-OK');
+              myDialog(context);
+//                        _scan(context);
+//                        Navigator.push(context,
+//                            MaterialPageRoute(builder: (context) => ewMPage()));
+            },
+            child: buttonContainer));
+  }
 }
